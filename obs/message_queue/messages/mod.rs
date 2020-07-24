1863376469e92162929e6b44a74615e0fa30fbbf
Mark <marktmmrmn@gmail.com>
2020-07-24 17:12:12 +0200

    Removed some obsolete code

--- obs/message_queue/messages/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod get_all_queue_counts;
-pub mod get_queue_count;
-pub mod rabbit_mq_status;
-
-pub use get_all_queue_counts::GetAllQueueCountsMessage;
-pub use get_queue_count::GetQueueCountMessage;
-pub use rabbit_mq_status::RabbitMqStatusMessage;
--- obs/message_queue/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod actor;
-pub mod messages;
-// pub mod utils;
